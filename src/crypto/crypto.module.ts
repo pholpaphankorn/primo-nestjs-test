386918dd4d8a1953e67b42f6825804586098ed3e
pholpaphankorn <phol12711@outlook.com>
2026-05-09 22:26:04 +0700

    feat: add crypto module

---
+++ src/crypto/crypto.module.ts
@@ -0,0 +1,9 @@
+import { CryptoController } from "./crypto.controller";
+import { CryptoService } from "./crypto.service";
+import { Module } from '@nestjs/common';
+
+@Module({
+  controllers: [CryptoController],
+  providers: [CryptoService],
+})
+export class CryptoModule {}
